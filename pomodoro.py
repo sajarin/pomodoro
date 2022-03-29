ca5b2abb5ae554c62694db0ad4727a8be93c4fdc
Shaedil Dider <shaedildider@gmail.com>
2022-03-28 22:34:56 -0400

    feat: track the number of tasks done per day
    A file called statistics.txt logs the number of tasks done per day.
    The script compares the most recent date logged and today's date and
    checks if it needs to add a new entry (see below) or update the tasks
    done during the day.
    
    Entry format:
    
    DATETIME, MM/DD/YY
    NUM_OF_TASKS_DONE, #

--- pomodoro.py
+++ pomodoro.py
@@ -19,11 +19,13 @@
         --version   Show version and exit
 """
 
+import os
 import time
 import random
 import argparse
 import tkinter as tk
 from tkinter import messagebox
+from datetime import date
 
 
 def main():
@@ -35,7 +37,12 @@ def main():
             'length_of_tasks_in_minutes': 0,
             'num_of_tasks_done': 0
             }
+    stats_contents = {
+            'datetime': "",
+            'num_of_tasks_done': 0
+            }
     config = "config.txt"
+    stats = "statistics.txt"
     with open(config, "r") as handle:
         lines = []
         for line in handle:
@@ -48,10 +55,28 @@ def main():
     task_length_in_seconds = int(
             60*config_contents['length_of_tasks_in_minutes'])
 
+    with open(stats, "r") as handle:
+        lines = []
+        raw_lines = handle.readlines()[-2:]
+        for line in raw_lines:
+            lines.append(line.split(",")[1].strip())
+        stats_contents['datetime'] = str(lines[0])
+        print(stats_contents['datetime'])
+        if is_today(stats_contents['datetime']):
+            stats_contents['num_of_tasks_done'] = int(lines[1])
+        else:
+            stats_contents['num_of_tasks_done'] = 0
+        print(stats_contents['num_of_tasks_done'])
+
     while True:
         run_timer(task_length_in_seconds)
         config_contents['num_of_tasks_done'] += 1
+        stats_contents['num_of_tasks_done'] += 1
         update_config(config_contents, config)
+        update_statistics(stats_contents, stats, config_contents[
+                                                          'num_of_tasks_done'])
+        # set new date to today's to avoid duplication of log entries
+        stats_contents['datetime'] = date.today().strftime("%m/%d/%y")
         reward_prob = get_reward_probability(config_contents)
         is_reward = check_reward(reward_prob)
         if is_reward:
@@ -62,7 +87,7 @@ def handle_args() -> None:
     # handle script arguments
     parser = argparse.ArgumentParser(description='A timer + rng for skinnerian\
                                      reinforcement')
-    args = parser.parse_args()
+    parser.parse_args()
 
 
 def run_timer(seconds: int) -> None:
@@ -103,6 +128,50 @@ def update_config(contents: dict, config: str) -> None:
         for elem in contents.keys():
             line = elem.upper() + ", " + str(contents[elem]).upper() + '\n'
             handle.write(line)
+
+
+def update_statistics(contents: dict, statistics: str, cur_tasks: int) -> None:
+    """
+        Writes to the statistics file the current number of tasks done and date
+    """
+    keys = list(contents.keys())
+    tasks_line = keys[1].upper() + ", " + str(contents['num_of_tasks_done']
+                                              ).upper() + '\n'
+    date_line = keys[0].upper() + ", " + date.today().strftime(
+                                                             "%m/%d/%y") + '\n'
+
+    # if date logged is today's, then edit 'num_of_tasks_done' line only
+    if is_today(contents['datetime']):
+        truncate_last_line_in_file(statistics)
+        with open(statistics, 'a', encoding='utf8') as handle:
+            handle.write('\n' + tasks_line)
+    # new date, new entry in log file and first task of the day
+    else:
+        tasks_line = keys[1].upper() + ", 1" + '\n'
+        with open(statistics, "a") as handle:
+            handle.write(date_line)
+            handle.write(tasks_line)
+
+
+def is_today(saved_datetime: str) -> bool:
+    """Checks if today is the same date as the one logged"""
+    if saved_datetime == date.today().strftime("%m/%d/%y"):
+        return True
+    return False
+
+
+def truncate_last_line_in_file(filename: str) -> None:
+    """Finds and removes the last line in a file"""
+
+    with open(filename, "rb+") as handle:
+        handle.seek(0, os.SEEK_END)
+        pos = handle.tell() - 1
+        while pos > 0 and handle.read(1) != b"\n":
+            pos -= 1
+            handle.seek(pos, os.SEEK_SET)
+        if pos > 0:
+            handle.seek(pos - 1, os.SEEK_SET)
+            handle.truncate()
 
 
 def get_reward_probability(config_contents: dict) -> float:
